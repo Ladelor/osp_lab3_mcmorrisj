6421341b4c21d215ad3d427fb3dac3ae5e3bb4f0
McMorris <mcmorrisj@debian-9.3.0-template>
2018-04-29 22:04:44 -0700

    Added commands.h and made falsh.c include it
    commands.h contains function prototypes for require built-in commands
    Functions: exit, pwd, cd, setpath, and help

---
+++ commands.h
@@ -0,0 +1,10 @@
+//pragma once is the same as the #ifndef, #define, #endif sequence
+//Fewer lines, less likely for mistakes, imo it should be standard now
+#pragma once
+
+//Not named exit to avoid conflict built-in function warning
+int exitProgram();
+int pwd();
+int cd(char**);
+int setpath(char**);
+int help();

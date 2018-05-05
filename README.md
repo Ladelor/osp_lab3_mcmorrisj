fe735d87c2ae8514be058d34a72455209906ad3e
McMorris <mcmorrisj@debian-9.3.0-template>
2018-05-04 18:20:39 -0700

    Fills in the readme, fixes a bug with redirection, and adds comments
    Readme basically just help.txt
    Bug occuring where the > and arg were being passed in as args to setpath and exec'ed commands
    Comments added in falsh.c

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# osp_lab3_mcmorrisj
+falsh is a unix shell interpreter. It has built in commands and can
+call other commands. Use simply by running falsh or ./falsh
+
+Built-in Commands:
+ - exit
+      Exits the program, no parameters
+ - pwd
+      Prints current working directory, no paramters
+ - cd [dir]
+      Change directory
+      No parameters changes to home directory
+      Single optional paramters changes to specified path
+ - setpath <dir> [dir] ... [dir]
+      Overwrites the path to one or more specified paths
+      DOES NOT check to make sure paths are valid
+ - help
+      Displays this screen
--- falsh.c
+++ falsh.c
@@ -8,53 +8,77 @@
 
 int main(int argc, char* argv[])
 {
+	//This means the user entered falsh -h or someother bad input
 	if(argc == 2)
 	{
+		//If they did falsh -h, print help.txt and exit
 		if(strstr(argv[1], "-h") != 0)
 		{
 			help();
 			return 0;
 		}
+		//Otherwise, invalid call
 		else
 		{
 			printf("Invalid falsh call, no parameters or -h flag only\n");
 			return -1;
 		}
 	}
+	//If there are more than two args, it was an invalid call
 	else if(argc > 2)
 	{
 		printf("Invalid falsh call, no parameters or -h flag only\n");
 		return -1;
 	}
 
-	//Reset PATH to only include /bin
+	//setenv sets an environment variable
+	//Passing in that we want to change "PATH"
+	//we are chaning it to "\bin", and we are overriding it
 	setenv("PATH", "/bin", 1);
 
+	//Some variables to deal with keeping track of stdout and strerr with redirection
 	int fdOUT = -1, fdERR = -1;
+	//dup copies a file descriptor are returns a copy of it
 	int OUTCopy = dup(STDOUT_FILENO);
 	int ERRCopy = dup(STDERR_FILENO);
 
+	//Program will exit on user exit command
 	while(1)
 	{
 		//Assuming max input length is 256
 		int maxLength = 256;
 		//cstring to store user entry
 		char userInput[maxLength];
+		//Just a boring message for good looks
 		printf("Welcome to the best shell! >> ");
+		//fgets reading from a filestream for some length and storing that string
+		//First param is buffer to store what is read
+		//Second param is the max length to read (we are assuming 256)
+		//Third param is what to read from, stdin
 		fgets(userInput, maxLength, stdin);
 
+		//This means only an enter was hit, just prompt the user again
 		if(strlen(userInput) == 1)
 			continue;
 
+		//Copying the input as strtok messes with the string given
 		char *userInputCopy = NULL;
+		//duplicate the userInput and store in a copy
 		userInputCopy = strdup(userInput);
+		//char** to store args like argv
+		//Max 100 args
 		char *args[100];
+		//Parameter to store arg count like argc
 		int argCount = 0;
+		//Go through the string copy until there are no more tokens
+		//Returns a token, stores in args, and adds to argCount
+		//Ends when there are no more tokens
 		while((args[argCount] = strtok(userInputCopy, " ")) != NULL)
 		{
 			argCount++;
 			userInputCopy = NULL;
 		}
+		//Free the memory created by the strdup to copy userInput
 		free(userInputCopy);
 
 		//Without this, have an excess new line at the end of the last argument
@@ -62,46 +86,73 @@ int main(int argc, char* argv[])
 		//In effect, this replaces the unwanted \n with a \0
 		args[argCount - 1][strlen(args[argCount - 1]) - 1] = '\0';
 
-		//int fdOUT, fdERR = -1;
-		//int OUTCopy = dup(STDOUT_FILENO);
-		//int ERRCopy = dup(STDERR_FILENO);
+		//Search through each arg to see if we will redirect
 		for(int i = 0; i < argCount; i++)
 		{
+			//This means we are going to attempt to redirect
 			if(strstr(args[i], ">") != 0)
 			{
+				//This means there is exactly one arg after >
+				//This is a valid redirect
 				if(i == (argCount - 2))
 				{
+					//Copy the argument for convenience
+					//In adding .out and .err
 					char* fileCopy = strdup(
 						args[argCount-1]);
+					//Closing stdout
 					close(STDOUT_FILENO);
+					//opening userparam + .out
 					fdOUT = open(strcat(fileCopy,".out"),
 					O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+					//closing stderr
 					close(STDERR_FILENO);
+					//opening userparam + .err
+					//Recopying to get rid of .out
 					strcpy(fileCopy, args[argCount - 1]);
 					fdERR = open(strcat(fileCopy,".err"),
 					O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+					//free the copy made by strdup
 					free(fileCopy);
+					//These lines make sure the correct args
+					//are passed to any program we exec
+					//i.e. get rid of the > and filePath
+					args[argCount - 1] = NULL;
+					args[argCount - 2] = NULL;
+					argCount -=2;
 				}
+				//This means there are more than one params
+				//after the >
 				else
 				{
 					printf("Invalid use of redirection \
 only one file argument allowed\n");
+					//Cheap and dirty way to identify
+					//the invalid redirection
 					argCount = -1;
 				}
 			}
 		}
 
+		//Redirection failed, leave
 		if(argCount == -1)
 			continue;
 
+		//Ignoring extra params on most commands
+		//Noticed bash did this in some cases so didn't check everything
+		//User entered exit so exit
 		if(strstr(args[0], "exit") != 0)
 		{
 			exitProgram();
 		}
+		//User entered pwd, so just run pwd
 		else if(strstr(args[0], "pwd") != 0)
 		{
 			pwd();
 		}
+		//User entered cd, if there are no args, move to HOME
+		//One arg, try and move to it
+		//Two+ args, print an error message
 		else if(strstr(args[0], "cd"))
 		{
 			if(argCount == 1)
@@ -111,46 +162,65 @@ only one file argument allowed\n");
 			else
 				printf("cd only accepts one argument\n");
 		}
+		//setpath has little to no error handling as per instructions
+		//It will make sure there is atleast one arg
+		//Give it effectively argc, argv
 		else if(strstr(args[0], "setpath"))
 		{
 			setpath(argCount, args);
 		}
+		//User entered help, prints help.txt
 		else if(strstr(args[0], "help"))
 		{
 			help();
 		}
+		//We will try to run any other input as an exec
 		else
 		{
+			//Fork a child process
 			int rc = fork();
+			//if rc is 0, the fork failed and something is bonked
+			//This is an error beyond our handling so just exit with error
 			if(rc < 0)
 			{
 				fprintf(stderr, "Fork failed\n");
 				exit(1);
 			}
+			//This is the child proccess
+			//Attempt to exec it
 			else if(rc == 0)
 			{
+				//Mostly using execvp to follow example from book
+				//Attempt to call the program with given args
+				//This will not return unless it can't run args[0]
 				execvp(args[0], args);
+				//Print the error message from the child
 				printf("%s command not found\n", args[0]);
+				//Exit the child allowing the parent to continue
 				exit(1);
 			}
 			else
 			{
+				//Wait for the child to finish the exec or exit after failing to exec
 				wait(NULL);
 			}
 		}
 
+		//This means we did redirection
 		if(fdOUT != -1)
 		{
+			//Close what we were redirecting stdOUT to
 			close(fdOUT);
+			//Copy stdouts fd back into the right slot
 			dup2(OUTCopy, STDOUT_FILENO);
-			close(OUTCopy);
+			//reset for the next command
 			fdOUT = -1;
+			//Same process as above for stderr
 			close(fdERR);
 			dup2(ERRCopy, STDERR_FILENO);
-			close(ERRCopy);
 			fdERR = -1;
 		}
 	}
-	//printf("hi\n");
+	//This should never actually run
 	return 0;
 }
